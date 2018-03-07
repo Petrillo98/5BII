6699f79f779023c4b47c5a334e93893a0bd41908
Niervuccio <32492046+Niervuccio@users.noreply.github.com>
2018-03-07 19:49:01 +0100

    Add files via upload

---
+++ LavoroGociman/Orienteering Gociman rivisto.php
@@ -0,0 +1,24 @@
+﻿<?php 
+	$xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?>'.'<orienteering xmlns="http://www.w3.org/2001/XMLSchema"></orienteering>');
+	$xml->addAttribute('version',"1");
+	$conn = mysqli_connect("localhost", "root", "", "my_orienteeringpontedera");
+	$serv = $xml->addChild('server');
+	$nam = $serv->addChild('name',"localhost");
+	$ver = $serv->addChild('version', "1");
+	$result = mysqli_query($conn,"SELECT id, CONCAT(nome,' ', cognome) as name, indirizzo as address, latitudine as lat, longitudine as lng, posizione as type FROM persone");
+	$user = $xml->addChild('markers');
+	$resdate=mysqli_query($conn,"SELECT DATE_FORMAT(NOW(), '%Y-%m-%dT%T') as data1");
+	$data = mysqli_fetch_array($resdate);
+	$date=$data[0];
+	$user->addAttribute("generated",$date);
+	while($row = mysqli_fetch_assoc($result)){
+		$marker = $user->addChild('marker');
+		foreach ($row as $key => $value) {
+			$marker->addAttribute($key, $value);
+		}
+	}
+	file_put_contents('orienteering.xml',$xml->asXML()); 
+	mysqli_free_result($result);
+	mysqli_free_result($resdate);
+	mysqli_close($conn);
+?>
